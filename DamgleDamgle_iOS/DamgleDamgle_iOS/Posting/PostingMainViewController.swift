b13b087812d4c1f9fe9ac63c79dad284b6937b29
최원석 <cws653@naver.com>
2022-07-02 17:07:44 +0900

    [add] Posting 뷰컨트롤러 및 스토리보드 추가

---
+++ DamgleDamgle_iOS/DamgleDamgle_iOS/Posting/PostingMainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PostingMainViewController.swift
+//  DamgleDamgle_iOS
+//
+//  Created by 최원석 on 2022/07/02.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class PostingMainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
