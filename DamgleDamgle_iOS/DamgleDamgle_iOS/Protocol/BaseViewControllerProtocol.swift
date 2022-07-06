fa178d85e1ae7b63dd2ca98689dd8eb32e72e01d
hope1053 <hope1053@naver.com>
2022-07-06 23:16:25 +0900

    ⚡ BaseViewController, CustomView 프로토콜 생성

---
+++ DamgleDamgle_iOS/DamgleDamgle_iOS/Protocol/BaseViewControllerProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  BaseViewProtocol.swift
+//  DamgleDamgle_iOS
+//
+//  Created by 최혜린 on 2022/07/06.
+//
+
+import Foundation
+
+protocol BaseViewController {
+    func layoutView()
+    func setUpView()
+}
---
+++ DamgleDamgle_iOS/DamgleDamgle_iOS/Protocol/CustomViewProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  CustomViewProtocol.swift
+//  DamgleDamgle_iOS
+//
+//  Created by 최혜린 on 2022/07/06.
+//
+
+import Foundation
+
+protocol BaseView {
+    func initView()
+    func setUpView()
+}
