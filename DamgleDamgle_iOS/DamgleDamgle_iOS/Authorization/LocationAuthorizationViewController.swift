b7c86071cfde3864a371918e731a5b2c61e21949
hope1053 <hope1053@naver.com>
2022-07-12 22:19:45 +0900

    🚀 위치 권한 업데이트 구현

--- DamgleDamgle_iOS/DamgleDamgle_iOS/Authorization/LocationAuthorizationViewController.swift
+++ DamgleDamgle_iOS/DamgleDamgle_iOS/Authorization/LocationAuthorizationViewController.swift
@@ -11,10 +11,13 @@ final class LocationAuthorizationViewController: UIViewController, BaseViewContr
     @IBOutlet private weak var guideLabel: UILabel!
     
     let fullDimView: UIView = FullDimView()
+    private let locationManager: LocationService = LocationService.shared
 
 // MARK: - override
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(appMovedToForeground), name: UIApplication.willEnterForegroundNotification, object: nil)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -22,6 +25,21 @@ final class LocationAuthorizationViewController: UIViewController, BaseViewContr
         
         layoutView()
         setUpView()
+        
+        locationManager.delegate = self
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        locationManager.delegate = nil
+        
+        NotificationCenter.default.removeObserver(self, name: UIApplication.willEnterForegroundNotification, object: nil)
+    }
+    
+// MARK: - objc
+    @objc func appMovedToForeground() {
+        locationManager.checkLocationServiceAuthorization()
     }
     
 // MARK: - UDF
@@ -34,5 +52,33 @@ final class LocationAuthorizationViewController: UIViewController, BaseViewContr
     
     func setUpView() {
         guideLabel.font = DamgleFont.bodyBold18.font
+    }
+    
+    func checkCurrentStatus(currentStatus: LocationAuthorizationStatus?) {
+        let title: String = "위치정보 이용에 대한 엑세스 권한이 없어요"
+        let message: String = "설정으로 이동해서 권한을 변경해주세요!"
+        
+        switch currentStatus {
+        case .authorizationDenied, .locationServiceDisabled:
+            self.showSingleAlertController(title: title, message: message) {
+                guard let url = URL(string: UIApplication.openSettingsURLString) else { return }
+                
+                if UIApplication.shared.canOpenURL(url) {
+                    UIApplication.shared.open(url)
+                }
+            }
+        case .success:
+            // TODO: push 권한 받는 뷰컨으로 이동
+            break
+        case .locationUpdateFail, .none:
+            break
+        }
+    }
+}
+
+// MARK: - extension
+extension LocationAuthorizationViewController: LocationDataProtocol {
+    func updateCurrentStatus(_ currentStatus: LocationAuthorizationStatus?) {
+        checkCurrentStatus(currentStatus: currentStatus)
     }
 }
