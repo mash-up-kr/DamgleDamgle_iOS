2a272559ebc98af6f0df429064dcf2fdafeb1eb1
hope1053 <hope1053@naver.com>
2022-07-17 17:50:14 +0900

    [#21] 🚀 PostStatus Enum 추가

--- DamgleDamgle_iOS/DamgleDamgle_iOS/Global/Define/UIViewController+Extension.swift
+++ DamgleDamgle_iOS/DamgleDamgle_iOS/Global/Define/UIViewController+Extension.swift
@@ -35,4 +35,64 @@ extension UIViewController {
         case single
         case double
     }
+    
+    enum PostStatus {
+        case inProgress
+        case success
+        case fail
+        
+        var statusTitle: String {
+            switch self {
+            case .inProgress:
+                return """
+                지금 담벼락에
+                나만의 글 남기는 중...
+                """
+            case .success:
+                return """
+                담벼락에
+                나만의 글 남기기 완료!
+                """
+            case .fail:
+                return """
+                담벼락에
+                담글 남기기 실패...
+                """
+            }
+        }
+        
+        var subTitle: String {
+            switch self {
+            case .inProgress, .success:
+                return ""
+            case .fail:
+                return """
+                네트워크 오류 등으로 글남기기를 실패했어요.
+                다음에 다시 시도해주세요.
+                """
+            }
+        }
+        
+        var buttonTitle: String? {
+            switch self {
+            case .inProgress:
+                return nil
+            case .success:
+                return "확인하러 가기"
+            case .fail:
+                return "다시 담글 남기러 가기"
+            }
+        }
+        
+        var image: UIImage? {
+            switch self {
+            case .inProgress:
+                return nil
+            case .success:
+                return UIImage(named: "post_success")
+            case .fail:
+                return UIImage(named: "post_fail")
+            }
+        }
+    }
 }
