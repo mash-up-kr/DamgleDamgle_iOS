c9521af402202a3084ed0d1f65844f715dafa1ee
최혜린 <hope1053@mhqglobal.com>
2022-07-29 11:30:25 +0900

    [#27] 🚀 Date Extension 코드 정리

--- DamgleDamgle_iOS/DamgleDamgle_iOS/Global/Extension/Date+Extension.swift
+++ DamgleDamgle_iOS/DamgleDamgle_iOS/Global/Extension/Date+Extension.swift
@@ -15,47 +15,20 @@ extension Date {
     var hourInSec: Int {
         60
     }
-    
-    // 이번 달 첫 날 구하기
-    func startOfCurrentMonth() -> Date {
-        Calendar.current.date(from: Calendar.current.dateComponents([.year, .month], from: Calendar.current.startOfDay(for: self)))!
-    }
-    
-    // 이번 달 마지막 날 구하기
-//    func endOfCurrentMonth() -> Date {
-//        Calendar.current.date(byAdding: DateComponents(month: 1, day: -1), to: self.startOfCurrentMonth())!
-//    }
-    
-    // 다음 달 첫 날 구하기
-    func startOfNextMonth() -> DateComponents {
-        let firstDayOfCurrentMonth = startOfCurrentMonth()
-        let firstDayOfNextMonth = Calendar.current.date(byAdding: DateComponents(month: 1), to: firstDayOfCurrentMonth)!
-        return Calendar.current.dateComponents([.year, .month, .day], from: firstDayOfNextMonth)
-    }
-    
-    // 다음 달 첫날 <-> 현재 time interval 구하기
-    func startDateOfNextMonth() -> TimeInterval {
-        let firstDateOfNextMonth: DateComponents = self.startOfNextMonth()
-        // Specify date components
-        var dateComponents = DateComponents()
-        dateComponents.year = firstDateOfNextMonth.year
-        dateComponents.month = firstDateOfNextMonth.month
-        dateComponents.day = firstDateOfNextMonth.day
-        dateComponents.timeZone = TimeZone(abbreviation: "KST") // Japan Standard Time
-        dateComponents.hour = 0
-        dateComponents.minute = 0
-        dateComponents.second = 0
 
-        let userCalendar = Calendar(identifier: .gregorian)
-        let startDateOfNextMonth = userCalendar.date(from: dateComponents)!
-        
-        let timeInterval = self.distance(to: startDateOfNextMonth)
-        return timeInterval
+    func startOfCurrentMonth() -> Date {
+        let currentMonthComponent = Calendar.current.dateComponents([.year, .month], from: Calendar.current.startOfDay(for: self))
+        return Calendar.current.date(from: currentMonthComponent)!
     }
     
-    // 인터벌 -> 타입으로 계산해주기
-    func getIntervalTilLastDate() -> (DateIntervalType, Int) {
-        let currentInterval = Int(self.startDateOfNextMonth())
+    func startOfNextMonth() -> Date {
+        let nextMonthInDate = Calendar.current.date(byAdding: DateComponents(month: 1), to: startOfCurrentMonth())!
+        let startOfNextMonth = Calendar.current.startOfDay(for: nextMonthInDate)
+        return startOfNextMonth
+    }
+    
+    func getDateIntervalType() -> (DateIntervalType, Int) {
+        let currentInterval = Int(distance(to: startOfNextMonth()))
         
         if currentInterval > dateInSec {
             return (.moreThanDay, Int(currentInterval / dateInSec))
