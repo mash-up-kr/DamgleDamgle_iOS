53ceec5715f920be5edd2e1fbfcb1205f8b81616
hope1053 <hope1053@naver.com>
2022-07-30 14:02:06 +0900

    [#35] ⚡ postStory request 보낼 때 사용할 model 구현

---
+++ DamgleDamgle_iOS/DamgleDamgle_iOS/Network/Story/Model/StoryRequest.swift
@@ -0,0 +1,14 @@
+//
+//  StoryRequest.swift
+//  DamgleDamgle_iOS
+//
+//  Created by 최혜린 on 2022/07/30.
+//
+
+import Foundation
+
+struct PostStoryRequest {
+    let lat: Double
+    let lng: Double
+    let content: String
+}
