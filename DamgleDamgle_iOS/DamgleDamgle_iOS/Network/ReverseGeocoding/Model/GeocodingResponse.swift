a03aeb283b14e67e46a2d6c45633e5ac0710b630
hope1053 <hope1053@naver.com>
2022-07-31 16:17:56 +0900

    [#37] ⚡ GeocodingResponse model 생성

---
+++ DamgleDamgle_iOS/DamgleDamgle_iOS/Network/ReverseGeocoding/Model/GeocodingResponse.swift
@@ -0,0 +1,104 @@
+//
+//  GeocodingResponse.swift
+//  DamgleDamgle_iOS
+//
+//  Created by 최혜린 on 2022/07/31.
+//
+
+// This file was generated from JSON Schema using quicktype, do not modify it directly.
+// To parse the JSON, add this file to your project and do:
+//
+//   let systemWeather = try? newJSONDecoder().decode(SystemWeather.self, from: jsonData)
+
+import Foundation
+
+// MARK: - SystemWeather
+struct GeocodingResponse: Codable {
+    let status: Status
+    let results: [ReverseResult]
+    
+    func getAddress() -> String {
+        var address: String
+        
+        guard let district = self.results.first?.region.area2.name else { return "" }
+        guard let town = self.results.first?.region.area3.name else { return "" }
+        guard let streetAddress = self.results.first?.land.name else { return "" }
+        
+        if streetAddress == "" {
+            address = "\(district) \(town)"
+        } else {
+            address = "\(town) \(streetAddress)"
+        }
+        
+        return address
+    }
+}
+
+// MARK: - Result
+struct ReverseResult: Codable {
+    let name: String
+    let code: Code
+    let region: Region
+    let land: Land
+}
+
+// MARK: - Code
+struct Code: Codable {
+    let id, type, mappingID: String
+
+    enum CodingKeys: String, CodingKey {
+        case id, type
+        case mappingID = "mappingId"
+    }
+}
+
+// MARK: - Land
+struct Land: Codable {
+    let type, number1, number2: String
+    let addition0, addition1, addition2, addition3: Addition
+    let addition4: Addition
+    let name: String
+    let coords: Coords
+}
+
+// MARK: - Addition
+struct Addition: Codable {
+    let type, value: String
+}
+
+// MARK: - Coords
+struct Coords: Codable {
+    let center: Center
+}
+
+// MARK: - Center
+struct Center: Codable {
+    let crs: String
+    let x, y: Double
+}
+
+// MARK: - Region
+struct Region: Codable {
+    let area0: Area
+    let area1: Area1
+    let area2, area3, area4: Area
+}
+
+// MARK: - Area
+struct Area: Codable {
+    let name: String
+    let coords: Coords
+}
+
+// MARK: - Area1
+struct Area1: Codable {
+    let name: String
+    let coords: Coords
+    let alias: String
+}
+
+// MARK: - Status
+struct Status: Codable {
+    let code: Int
+    let name, message: String
+}
