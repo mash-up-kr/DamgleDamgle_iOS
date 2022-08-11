1e45ffe1c1faeb71416b7107b393b1b66920244d
최원석 <cws653@naver.com>
2022-08-11 11:24:48 +0900

    🎨 listView API 추가

--- DamgleDamgle_iOS/DamgleDamgle_iOS/Posting/PostingMainViewController.swift
+++ DamgleDamgle_iOS/DamgleDamgle_iOS/Posting/PostingMainViewController.swift
@@ -103,7 +103,7 @@ extension PostingMainViewController: UITableViewDataSource {
         if apiState == APIState.error {
             return 1
         }
-        return testViewModel2.postModels.count
+        return viewModel.postModels?.stories.count ?? 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -118,13 +118,11 @@ extension PostingMainViewController: UITableViewDataSource {
         cell.setupTestUI(viewModel: testViewModel)
         cell.addSelectedIcon = { [weak self] reaction in
             guard let self = self else { return }
-            //            self.viewModel.addIconInModel(original: viewModel, icon: iconButton)
             guard let id = testViewModel?.id else { return }
             self.viewModel.postReaction(storyID: id, type: reaction.rawValue)
         }
         cell.deleteSeletedIcon = { [weak self] in
             guard let self = self else { return }
-            //            self.viewModel.deleteIconInModel(original: viewModel, icon: iconsButton)
             guard let id = testViewModel?.id else { return }
             self.viewModel.deleteReaction(storyID: id)
         }
