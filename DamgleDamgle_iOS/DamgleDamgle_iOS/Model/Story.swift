70b534347f48e132726546cdeafa2be29b9183ed
최원석 <cws653@naver.com>
2022-08-20 23:50:57 +0900

    ⚡ listViewAPI 추가

--- DamgleDamgle_iOS/DamgleDamgle_iOS/Model/Story.swift
+++ DamgleDamgle_iOS/DamgleDamgle_iOS/Model/Story.swift
@@ -15,31 +15,33 @@ struct Story: Decodable, Hashable, Identifiable {
     let id: String
     let userNo: Int
     let nickname: String
-    let x: Int
-    let y: Int
+    let x: Double
+    let y: Double
     let content: String
     let reactions: [Reaction]
-//    let reposrts: [Report]
+    let reactionSummary: [ReactionSummary]
+    let reactionOfMine: ReactionOfMine?
+    let reports: [Report]
     let createdAt, updatedAt: Int
 
     var secCreateAt: Double {
         return Double(createdAt) / 1000
     }
-    
+
     var secUpdatedAt: Double {
         return Double(updatedAt) / 1000
     }
     
     var offsetTimeText: String {
         let offsetTime = Calendar.current.dateComponents([.year,.month,.weekday,.day,.hour,.minute], from: secCreateAt.toDate ?? Date(), to: Date())
-        
+
         let year = offsetTime.year ?? 0
         let month = offsetTime.month ?? 0
         let week = Int((offsetTime.day ?? 0) / 7)
         let day = (offsetTime.day ?? 0) % 7
         let hour = offsetTime.hour ?? 0
         let minute = offsetTime.minute ?? 0
-        
+
         if year != 0 {
             return "\(year)년 전"
         } else if month != 0 {
@@ -53,5 +55,13 @@ struct Story: Decodable, Hashable, Identifiable {
         } else {
             return "\(minute)분 전"
         }
+    }
+    
+    var reactionAllCount: Int {
+        var allCount = 0
+        for reaction in reactionSummary {
+            allCount += reaction.count
+        }
+        return allCount
     }
 }
