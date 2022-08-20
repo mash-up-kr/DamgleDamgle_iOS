615d9d53616ebd67ea427e803b5612c6be3df1e1
최원석 <cws653@naver.com>
2022-08-20 15:54:13 +0900

    ✨pr내용 반영

---
+++ DamgleDamgle_iOS/DamgleDamgle_iOS/Model/Report.swift
@@ -0,0 +1,13 @@
+//
+//  Report.swift
+//  DamgleDamgle_iOS
+//
+//  Created by 최원석 on 2022/08/19.
+//
+
+import Foundation
+
+struct Report: Decodable, Hashable {
+    let userNo: Int
+    let createdAt: Int
+}
--- DamgleDamgle_iOS/DamgleDamgle_iOS/Posting/PostingViewModel.swift
+++ DamgleDamgle_iOS/DamgleDamgle_iOS/Posting/PostingViewModel.swift
@@ -8,11 +8,11 @@
 import Alamofire
 import Foundation
 
-final class TestPostingViewModel2 {
+final class PostingViewModel {
     
     private(set) var postModels: MyStoryResponse?
     
-    func getMyStory(size: Int?, storyID: String?,completion: @escaping (Bool) -> Void) {
+    func getMyStory(size: Int?, storyID: String?, completion: @escaping (Bool) -> Void) {
         StoryService.getMyStory(size: size, storyID: storyID) { result in
             switch result {
             case .success(let response):
@@ -20,30 +20,38 @@ final class TestPostingViewModel2 {
                 completion(true)
             case .failure(let error):
                 completion(false)
-                print(error)
             }
         }
     }
     
-    func postReaction(storyID: String, type: String) {
+    func postReaction(storyID: String, type: String, completion: @escaping (Bool) -> Void) {
         StoryService.postReaction(storyID: storyID, type: type) { result in
             switch result {
             case .success(let response):
-                print(response)
+                completion(true)
             case .failure(let error):
-                print(error)
+                completion(false)
             }
         }
     }
     
-    func deleteReaction(storyID: String) {
+    func deleteReaction(storyID: String, completion: @escaping (Bool) -> Void) {
         StoryService.deleteReaction(storyID: storyID) { result in
             switch result {
             case .success(let response):
-                print(response)
+                completion(true)
             case .failure(let error):
-                print(error)
+                completion(false)
             }
         }
+    }
+    
+    func sortTime() {
+        postModels?.stories.sort(by: { $0.createdAt > $1.createdAt })
+    }
+    
+    func sortPopularity() {
+        // TODO: 서버로부터 좋아요 숫자 Response를 받은 후 로직을 만들 예정
+        postModels?.stories.sort(by: { $0.id > $1.id })
     }
 }
